e6888cebb9dc6ce3a02673fa199694aaa660e987
Abbashozefa <abbashozefa@gmail.com>
2022-09-12 00:21:24 +0530

    js not working in django

--- waste_radar/static/slider.js
+++ waste_radar/static/slider.js
@@ -1,9 +1,13 @@
 var slideImg=document.getElementById("slideImg");
 var images=new Array();
-images[0]="{% static 'images\image1.jpeg' %}";
-images[1]="{% static 'images\images2.jpeg' %}";
-images[2]="{% static 'images\images3.jpeg' %}";
-images[3]="{% static 'images\images4.jpeg' %}";
+images[0] = new Image();
+images[0].src="{% static 'images\image1.jpeg' %}";
+images[1] = new Image();
+images[1].src="{% static 'images\images2.jpeg' %}";
+images[2] = new Image();
+images[2].src="{% static 'images\images3.jpeg' %}";
+images[3] = new Image();
+images[3].src="{% static 'images\images4.jpeg' %}";
 var len=images.length;
 var i=0;
 function slider(){
